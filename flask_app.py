9d821714db5f36203a04d0be4fde2db53ecc47dc
kevinmcalear <kevin.mcalear@gmail.com>
2014-10-19 23:19:34 -0400

    Added in my data files, my pickles, my requirements file, and the basic guts of my working python app.

--- flask_app.py
+++ flask_app.py
@@ -2,15 +2,100 @@
 # TODO now let's add a form that we can used to submit lyrics.
 # TODO import request from flask
 from flask import Flask, render_template, request
-from msd.stem import transformLyrics
+# Setting up a connection to the internetz
+from urllib2 import urlopen
+# Getting things to come back from json to a dict
+import json
+# Making that json pretty if needed
+from pprint import pprint
+# Dealing with stupid Unicode issues
+from django.utils.encoding import smart_str
+# Bring me my Pickles, slave!
 from sklearn.externals import joblib
+# Importing my standard stuff
+import pandas as pd
+import numpy as np
+
+
+
+# Building Out Some Functions to Ping The Hacker News API and Return Back Usable Lists Of Comments For Classifying
+
+# Get all of a user's comments
+def get_user_comments(username):
+    comments = []
+
+    url_usr_strt = "https://hacker-news.firebaseio.com/v0/user/"
+    url_itm_strt = "https://hacker-news.firebaseio.com/v0/item/"
+    url_end = ".json"
+    user = urlopen( url_usr_strt+username+url_end )
+    user = json.loads( user.read() )
+    if len(user['submitted']) > 100:
+        for c in user['submitted'][:101]:
+            item = urlopen( url_itm_strt+str(c)+url_end )
+            json_item = json.loads( item.read() )
+            if 'text' in json_item:
+                comments.append(smart_str(json_item['text']))
+    return comments
+
+
+# Get a final Hater Score. 100 is the worst, 0 is the best.
+def calculate_score(predictions):
+    total_score = []
+    for s in predictions:
+        total_score.append(s[1])
+
+    return np.mean(total_score) * 100
+
+# Get all a users comments and run them through my model
+def user_score(username, my_vect, clf):
+    comments = filter(None, get_user_comments(username))
+    badwords = set(pd.read_csv('data/my_badwords.csv').words)
+    badwords_count = []
+
+    for el in comments:
+        tokens = el.split(' ')
+        badwords_count.append(len([i for i in tokens if i.lower() in badwords]))
+
+    n_words = [len(c.split()) for c in comments]
+    allcaps = [np.sum([w.isupper() for w in comment.split()]) for comment in comments]
+    allcaps_ratio = np.array(allcaps) / np.array(n_words, dtype=np.float)
+    bad_ratio = np.array(badwords_count) / np.array(n_words, dtype=np.float)
+    exclamation = [c.count("!") for c in comments]
+    addressing = [c.count("@") for c in comments]
+    spaces = [c.count(" ") for c in comments]
+
+    re_badwords = np.array(badwords_count).reshape((len(badwords_count),1))
+    re_n_words = np.array(n_words).reshape((len(badwords_count),1))
+    re_allcaps = np.array(allcaps).reshape((len(badwords_count),1))
+    re_allcaps_ratio = np.array(allcaps_ratio).reshape((len(badwords_count),1))
+    re_bad_ratio = np.array(bad_ratio).reshape((len(badwords_count),1))
+    re_exclamation = np.array(exclamation).reshape((len(badwords_count),1))
+    re_addressing = np.array(addressing).reshape((len(badwords_count),1))
+    re_spaces = np.array(spaces).reshape((len(badwords_count),1))
+
+    vect = my_vect.transform(comments)
+    features = np.hstack((vect.todense(), re_badwords))
+    features = np.hstack((features, re_n_words))
+    features = np.hstack((features, re_allcaps))
+    features = np.hstack((features, re_allcaps_ratio))
+    features = np.hstack((features, re_bad_ratio))
+    features = np.hstack((features, re_exclamation))
+    features = np.hstack((features, re_addressing))
+    features = np.hstack((features, re_spaces))
+    predictions = clf.predict_proba(features)
+
+    return calculate_score(predictions)
+
+
+
 
 app = Flask(__name__)
 
 
-print 'Loading model...'
-pipeline = joblib.load('lyrics_pipeline.pkl')
-print 'Model loaded!'
+print 'Loading clf & vect...'
+vect = joblib.load('vect.pkl')
+clf = joblib.load('clf.pkl')
+print 'All loaded Captn\'!'
 
 # TODO add a view for the route '/' that renders the template 'lyrics_form.html'
 # TODO inspect the template 'lyrics_form.html'
@@ -19,18 +104,20 @@ def display_form():
     return render_template('lyrics_form.html')
 # TODO bind this view to the route '/predictions'
 # TODO set this view to use HTTP POST only
-@app.route('/predictions', methods=['POST'])
+@app.route('/hater-score', methods=['POST'])
 def predict_artist():
     # TODO get the lyrics from the body of the POST request
-    lyrics = request.form['lyrics']
-    lyrics = transformLyrics(lyrics)
-    print 'predicting artist for %s' % lyrics
-    prediction = pipeline.predict([lyrics])
+    username = request.form['username']
+    comments = get_user_comments(username)
+    score = user_score(username, vect, clf)
+
+    print 'predicting hater score for %s' % username
     d = {
-        'lyrics': lyrics,
-        'artist': prediction[0]
+        'username': username,
+        'comments': comments,
+        'score': score
     }
-    return render_template('prediction.html', d=d)
+    return render_template('hater-score.html', d=d)
 
 if __name__ == '__main__':
     app.debug = True
--- its_pkl_time.py
+++ its_pkl_time.py
@@ -1,31 +1,82 @@
-# (Re)Training the model on application startup is wasteful.
-# Let's train a model and save it to disk.
-# The pickle module implements a fundamental, but powerful algorithm
-# for serializing and de-serializing a Python object structure.
-# Pickling is the process whereby a Python object hierarchy is
-# converted into a byte stream, and unpickling is the inverse
-# operation, whereby a byte stream is converted back into an object
-# hierarchy. Pickling (and unpickling) is alternatively known as
-# serialization, marshalling, or flattening, however,
-# to avoid confusion, the terms used here are pickling and unpickling.
-# sklearn provides a modified pickler that works better with large NumPy arrays.
+# This is the file I use to create my pkl file.
+# I use this so I can just load up my already trained an prepaired model so I don't have to do it every time.
+
+# IT'S FREAKING PICKLE TIME!
+print 'Training my model...'
+print 'PICKLE TIME!!!!!!!!!'
+
+# Importing my standard stuff
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import Pipeline
-
-print 'Training model...'
-df = pd.read_csv('lyrics.csv')
-y = df['Artist']
-X = df['Lyrics']
-
-pipeline = Pipeline([
-    ('vect', TfidfVectorizer()),
-    ('clf', LogisticRegression())
-])
-pipeline.fit(X, y)
-
-# TODO import joblib from sklearn.externals
+# Setting up my pickling skillz, yo
 from sklearn.externals import joblib
-# TODO dump the fitted pipeline to a pickle called lyrics_pipeline.pkl
-joblib.dump(pipeline, 'lyrics_pipeline.pkl')
+
+# Here I combined my training & test data. I will used this as main total "training" data in production & for cross_val stuff.
+corpus = pd.read_csv('data/total_data.csv')
+# Getting our list of "badwords"
+badwords = set(pd.read_csv('data/my_badwords.csv').words)
+
+
+# Setting Up my X & y
+X_train = corpus.Comment
+y_train = corpus.Insult
+
+# Here I added addtional features
+
+# This is just a count of how many bad words
+train_badwords_count = []
+test_badwords_count = []
+
+for el in X_train:
+    tokens = el.split(' ')
+    train_badwords_count.append(len([i for i in tokens if i.lower() in badwords]))
+
+# **SHOUT OUT**
+# I was messing with stuff from Andreas Mueller for these next features. Thanks man! :)
+# His Blog Post on this: http://blog.kaggle.com/2012/09/26/impermium-andreas-blog/
+# **SHOUT OUT**
+train_n_words = [len(c.split()) for c in X_train]
+train_allcaps = [np.sum([w.isupper() for w in comment.split()]) for comment in X_train]
+train_allcaps_ratio = np.array(train_allcaps) / np.array(train_n_words, dtype=np.float)
+train_bad_ratio = np.array(train_badwords_count) / np.array(train_n_words, dtype=np.float)
+train_exclamation = [c.count("!") for c in X_train]
+train_addressing = [c.count("@") for c in X_train]
+train_spaces = [c.count(" ") for c in X_train]
+
+# Setting up or vect and clf instances
+vect = CountVectorizer(binary=True)
+clf = LogisticRegression()
+
+# Fitting and transforming my X_Train using Count Vectorizer
+X_train_transform = vect.fit_transform(X_train)
+
+# Reshaping my new features so I can stick them in my X_train
+train_reshaped_badwords = np.array(train_badwords_count).reshape((len(train_badwords_count),1))
+train_reshaped_n_words = np.array(train_n_words).reshape((len(train_badwords_count),1))
+train_reshaped_allcaps = np.array(train_allcaps).reshape((len(train_badwords_count),1))
+train_reshaped_allcaps_ratio = np.array(train_allcaps_ratio).reshape((len(train_badwords_count),1))
+train_reshaped_bad_ratio = np.array(train_bad_ratio).reshape((len(train_badwords_count),1))
+train_reshaped_exclamation = np.array(train_exclamation).reshape((len(train_badwords_count),1))
+train_reshaped_addressing = np.array(train_addressing).reshape((len(train_badwords_count),1))
+train_reshaped_spaces = np.array(train_spaces).reshape((len(train_badwords_count),1))
+
+# Adding my new features into my X_train
+X_train_transform = np.hstack((X_train_transform.todense(), train_reshaped_badwords))
+X_train_transform = np.hstack((X_train_transform, train_reshaped_n_words))
+X_train_transform = np.hstack((X_train_transform, train_reshaped_allcaps))
+X_train_transform = np.hstack((X_train_transform, train_reshaped_allcaps_ratio))
+X_train_transform = np.hstack((X_train_transform, train_reshaped_bad_ratio))
+X_train_transform = np.hstack((X_train_transform, train_reshaped_exclamation))
+X_train_transform = np.hstack((X_train_transform, train_reshaped_addressing))
+X_train_transform = np.hstack((X_train_transform, train_reshaped_spaces))
+
+# Fitting our Logisitc Regression Classifier
+clf.fit(X_train_transform,y_train)
+
+
+
+# Actually creating my pickle files for later use. One for my classifier and one for my vectorizerT
+joblib.dump(clf, 'clf.pkl')
+joblib.dump(vect, 'vect.pkl')
